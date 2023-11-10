3d5f0f7f1472990b9ec0f07346146dfa95e1b1d8
miguelmoranv <miguelonchas001@gmail.com>
2023-11-10 16:31:27 -0600

    v2

--- hola_mundo/settings.py
+++ hola_mundo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sv0@wpf3z0^kn5cg7y&lqf8wd62@mb+&%dx=(l(8*(8zol2p9^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.141.34.188']
 
 
 # Application definition
